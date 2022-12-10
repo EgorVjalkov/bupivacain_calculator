7de8985350b0cb17b446acf3ca754551eced3eaa
EgorVjalkov <e.vjalkov@yandex.ru>
2022-12-10 20:52:19 +0300

    add a sum and round of height

--- input_patient.py
+++ input_patient.py
@@ -15,11 +15,14 @@ class FindCount:
 class Patient:
 
 	def __init__(self):
-		self.patient_name = input('Имя?\n')
-		self.height = int(input('Рост в см?\n')) / 100
-		self.weight = int(input('Вес в кг?\n'))
-		self.bmi = round(self.weight/pow(self.height, 2), 1)
-
+		self.patient_name = input('pregnant`s name?\n')
+		self.height = int(input('pregnant`s height?\n'))
+		self.round_height = self.height
+		while self.round_height % 5 > 0:
+			self.round_height += 1
+		self.weight = int(input('pregnant`s weight?\n'))
+		self.bmi = round(self.weight/pow(self.height/100, 2), 1)
+		self.factors_list = []
 		# bmi
 		bmi_interpretation_dict = {
 			18.5: {'inter': 'deficit', 'count': -1},
@@ -33,6 +36,7 @@ class Patient:
 			if self.bmi < k:
 				self.bmi_count = bmi_interpretation_dict[k]['count']
 				self.bmi_interpretation = bmi_interpretation_dict[k]['inter']
+				self.factors_list.append(self.bmi_count)
 				break
 
 		fetus_weight_dict = {
@@ -43,28 +47,37 @@ class Patient:
 		fetus = FindCount(fetus_weight_dict, 'fetus is')
 		self.fetus = fetus.answer
 		self.fetus_count = fetus.count
+		self.factors_list.append(self.fetus_count)
 
 		bladder_condition = {
 			'r': {'inter': 'raptured or oligohydramnios', 'count': 0},
-			'n': {'inter': 'whole, no polyhydramnios', 'count': 0},
-			'p': {'inter': 'whole, polyhydramnios', 'count': 1},
+			'n': {'inter': 'intact, no polyhydramnios', 'count': 0},
+			'p': {'inter': 'intact, polyhydramnios', 'count': 1},
 		}
 		bladder = FindCount(bladder_condition, 'bladder is')
 		self.bladder = bladder.answer
 		self.bladder_count = bladder.count
+		self.factors_list.append(self.bladder_count)
 
 		back_discomfort_dict = {
-			'y': {'inter': 'discomfort lying on the back', 'count': 1},
-			'n': {'inter': 'NOT discomfort lying on the back', 'count': 0}
+			'y': {'inter': 'discomfort in the position on the back', 'count': 1},
+			'n': {'inter': 'NOT discomfort in the position on the back', 'count': 0}
 		}
 		back = FindCount(back_discomfort_dict, 'pregnant has')
 		self.back_discomfort = back.answer
 		self.back_discomfort_count = back.count
+		if sum(self.factors_list) < 0:
+			self.factors_list.append(self.back_discomfort_count)
 
-		print(self.bmi, self.bmi_count, self.bmi_interpretation)
+		self.sum_factors = sum(self.factors_list)
+
+		print(self.rounded_height, self.bmi, self.bmi_count, self.bmi_interpretation)
 		print(self.fetus, self.fetus_count)
 		print(self.bladder, self.bladder_count)
 		print(self.back_discomfort, self.back_discomfort_count)
+		print(self.sum_factors)
+
+
 
 
 a = Patient()
